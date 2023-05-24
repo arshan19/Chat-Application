10983c7fe8ba1d41efaffeed3d60178cfa838bf8
Arshan Ali <you@example.comaliarshan1934@gmail.com>
2023-05-24 15:14:22 +0530

    added socket.io and also added them in controllers

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
+const http = require('http');
 const connectDB = require('./db/connectDB');
 const routes = require('./routes/AllRoutes');
 const errorHandler = require('./middleware/errorHandler');
+const socketLogic = require('./socket/socketLogic');
+
 require('dotenv').config();
 
-
 const app = express();
-
+const server = http.createServer(app);
 
 // Body parsing middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 // Use the routes
 app.use('/', routes);
@@ -22,8 +23,11 @@ app.use(errorHandler);
 // MongoDB connection
 connectDB(process.env.MONGO_URI);
 
+// Socket.IO connection
+socketLogic(server);
+
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
